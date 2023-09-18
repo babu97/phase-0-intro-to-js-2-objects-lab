9c3e2b22301d16d98c9219f11d0cdd5a9ea1a3b0
babu97 <benkaimugul@gmail.com>
2023-09-18 14:59:50 +0300

    finished my assignment on objects

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// Write your solution in this file!
+
+let employee = {
+    name: "Ben Kaimogul",
+    streetAddress: "Nairobi Kenya",
+}
+
+function updateEmployeeWithKeyAndValue(employee,key, value){
+let newEmployee = {...employee};
+newEmployee[key]=value;
+return newEmployee;
+
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key, value){
+    employee[key] =value;
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+
+    const newEmployee = {...employee};
+     delete newEmployee[key]
+     return newEmployee;
+
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return  employee
+}
